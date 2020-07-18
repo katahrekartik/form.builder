07929a82c346f57ad4e33ada15a87c5d7ca7421a
2016118@iiitdmj.ac.in <39498672+katahrekartik@users.noreply.github.com>
2020-07-18 22:18:08 +0530

    phase 2 complete

--- script.js
+++ script.js
@@ -49,12 +49,30 @@ $(document).ready(function() {
 
 
     $('#fullname').click(function() {
-        var html = '<li class="placeholder-inner-content" id="header-component component">This is new item</li>';
+        var html = '<li class="placeholder-inner-content" id="fullname-component component"><h6>Name</h6><div class="firstname" style="display:inline-block"><input type="text" style="height:30px; width:150px;"></input><p>Firtname</p></div><div class="lastname" style="display:inline-block;margin-left:20px;"><input type="text" style="height:30px; width:150px;"></input><p>Lastname</p></div></li>';
         $('#form-container').append(html);
         scrollwindow();
-
-
     })
+
+    $('#phone').click(function() {
+        var html = '<li class="placeholder-inner-content" id="phone-component component"><h6>Phone</h6><div class="areacode" style="display:inline-block"><input type="text" style="height:30px; width:150px;"></input><p>Area Code</p></div><div class="phone-number" style="display:inline-block;margin-left:20px;"><input type="text" style="height:30px; width:150px;"></input><p>Phone Number</p></div></li>';
+        $('#form-container').append(html);
+        scrollwindow();
+    })
+
+    $('#email').click(function() {
+        var html = '<li class="placeholder-inner-content" id="email-component component"><h6>Email</h6><div class="email"><input type="text" style="height:30px; width:150px;"></input><p>example@domain.com</p></div></li>';
+        $('#form-container').append(html);
+        scrollwindow();
+    })
+
+
+    $('#address').click(function() {
+        var html = '<li class="placeholder-inner-content" id="address-component component"><h6>Address</h6><div class="street-address"><input type="text" style="height:30px; width:320px;"></input><p>Street Address</p></div><div class="city" style="display:inline-block"><input type="text" style="height:30px; width:150px;"></input><p>City</p></div><div class="state" style="display:inline-block;margin-left:20px;"><input type="text" style="height:30px; width:150px;"></input><p>State</p></div><div class="zip-code"><input type="text" style="height:30px; width:320px;"></input><p>Postal/Zip code</p></div></li>';
+        $('#form-container').append(html);
+        scrollwindow();
+    })
+
 
     //Apply border and display operations sections on selected element and remove from other elements
     $('#form-container').on('click', 'li', function() {
@@ -64,7 +82,6 @@ $(document).ready(function() {
         var lengthOfLi = $(this).find('.operations').length;
         if (lengthOfLi < 1) {
             $(this).append('<div class="operations" style="font-size:25px;float:right"><i class="fa fa-cog properties" style="color:black"></i><i class="fa fa-trash-o remove-component";color:red"></i></div>')
-
         }
         $(this).siblings('li').find('.operations').remove();
 
